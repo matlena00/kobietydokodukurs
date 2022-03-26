d44f71f800ec0100b0bb8c0ead36ffb903c4af59
matlena00 <mateusz.matlega@gmail.com>
2022-03-27 00:39:42 +0100

    Lekcja 03 i 04 Zadanie domowe - Praca z klasą Scanner, wyrażenia regularne, obługa wyjątków - od zera do junior java developera ;) ;)

--- src/main/java/pl/kobietydokodu/koty/Interfejs.java
+++ src/main/java/pl/kobietydokodu/koty/Interfejs.java
@@ -1,6 +1,11 @@
 package pl.kobietydokodu.koty;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Interfejs {
 
@@ -18,9 +23,38 @@ public class Interfejs {
         System.out.println("Podaj imie opiekuna: ");
         kot.setImieOpiekuna(getUserInput());
 
-        System.out.println("Pomyślnie dodano dane ;)");
+
+        Pattern wzorzecDaty = Pattern.compile("[0-9]{4}.[0-1]?[0-9].[0-3]?[0-9]");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        String dataUrodzeniaWczytana;
+        do {
+            System.out.print("Podaj datę urodzenia kota w formacie RRRR.MM.DD: ");
+            dataUrodzeniaWczytana = getUserInput();
+            if (wzorzecDaty.matcher(dataUrodzeniaWczytana).matches()) {
+                try {
+                    kot.setData(sdf.parse(dataUrodzeniaWczytana));
+                } catch (ParseException pe) {
+                    System.out.println("Coś jest nie tak z datą! Przykładowa data: 2014.01.05");
+                }
+            }
+        } while (kot.getData()==null);
+
+        Pattern wzorzecWagi = Pattern.compile("[0-9]+(\\.[0-9]+)?");
+        String wagaWczytana;
+        do {
+            System.out.print("Podaj wagę kota: ");
+            wagaWczytana = getUserInput();
+
+            if (wzorzecWagi.matcher(wagaWczytana).matches()) {
+                kot.setWaga(Float.valueOf(wagaWczytana));
+            }
+        } while (kot.getWaga() == null);
+
+
+
 
     }
+
 
     public static String getUserInput() {
         return sc.nextLine();
